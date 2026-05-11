59446f4f2819eac5ed3ca956ea3732dd5eeb2b6e
sopanbukhari <sopanbukhari@gmail.com>
2026-05-11 14:35:42 +0700

    login

--- login.cy.js
+++ login.cy.js
@@ -12,12 +12,4 @@ describe('SauceDemo E2E Login Test', () => {
     cy.url().should('include', '/inventory.html')
     cy.get('.title').should('have.text', 'Products')
   })
-
-  it('should show error for locked_out_user', () => {
-    cy.get('[data-test="username"]').type('locked_out_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()
-
-    cy.get('[data-test="error"]').should('be.visible').and('contain', 'Sorry, this user has been locked out.')
-  })
 })
